8ea948105b2f34949b07983e2c39eaa13d8028c3
Rithin Nagaraj <rithin.nagaraj@gmail.com>
2025-06-07 00:12:01 +0530

    Commented the app function

--- backend/app.py
+++ backend/app.py
@@ -102,7 +102,10 @@ def chat():
 
     return jsonify({"answer": answer})
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Default port to 10000 locally
-    app.run(host="0.0.0.0", port=port, debug=True)
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 10000)) 
+#     app.run(host="0.0.0.0", port=port, debug=True)
+
+# if __name__ == '__main__':
+#    app.run(debug=True)
 
